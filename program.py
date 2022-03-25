a0311ba7a845ea6220f1d89a18cf0f242398febb
Magne Slåtsveen <magne.slatsveen@ntnu.no>
2022-03-25 10:29:43 +0100

    Alle brukerhistorier dekket
    Co-authored-by: Are T <tommeras@users.noreply.github.com>

--- program.py
+++ program.py
@@ -1,45 +1,134 @@
 from datetime import date
+from json.tool import main
 import sqlite3
 
 #Her lager vi connection
 con = sqlite3.connect("kaffe.db")
 cursor = con.cursor()
 
+def lagBruker():
+    print("""Under kommer felt du må fylle ut for å lage en bruker: 
+        """)
+    epost = input("Skriv inn din e-post?: ")
+    passord = input("Skriv inn ønsket passord: ")
+    fulltnavn = input("Skriv inn ditt fulle navn(Kan være blank): ")
+    try:
+
+
+        cursor.execute(f'''INSERT INTO Bruker
+                VALUES ("{epost}", "{passord}", "{fulltnavn}")''')
+
+        con.commit()
+
+        cursor.execute(f'''SELECT Bruker.Epost, Bruker.FulltNavn FROM Bruker
+                        WHERE Bruker.Epost Like "{epost}"''')
+        
+        res = cursor.fetchall()
+        result = res[0]
+        print(f"""
+Du har nå laget en bruker med eposten: '{result[0]}'.
+Passordet ditt er: ***** (Dette er hemmelig dessverre så håper du husker)
+Ditt fulle navn er: {result[1]}.
+            """)
+    except:
+        print("Du har skrevet inn en allerede eksisterende bruker. Bruk en annen epost og prøv på nytt.")
+        lagBruker()
+
+def getBrenneriId(brenneri):
+    try:
+        cursor.execute(f"""SELECT BrenneriId from Brenneri 
+                        WHERE Brenneri.Navn like '{brenneri}'
+                    """)
+        result = cursor.fetchone()
+        return  result[0]
+    except:
+        print("Brennerinavnet er ikke i databasen, sender deg tilbake til starten.")
+        FørsteBrukerHistorie()
+
+def getKaffeId(kaffe, brenneriId):
+    try:
+        cursor.execute(f"""SELECT KaffeId from Kaffe 
+                            INNER JOIN Brenneri on Brenneri.BrenneriId = Kaffe.BrenneriId
+                            WHERE Kaffe.Navn like '{kaffe}' AND Brenneri.BrenneriId = {brenneriId}""")
+        result = cursor.fetchone()
+        return result[0]
+    except:
+        print("Kaffenavnet er enten ikke i databasen, eller så har brenneriet du skrev ikke laget en kaffe kalt dette, sender deg tilbake til starten.")
+        FørsteBrukerHistorie()
+
+def skrivNotat(epost, kaffeId, poeng, notat):
+    try:
+        datoStreng = date.today().isoformat()
+        dato = datoStreng.split("-")
+        år = int(dato[0])
+        måned = int(dato[1])
+        dag = int(dato[2])
+
+        cursor.execute(f'''INSERT INTO Kaffesmaking (Smaksnotat, Poeng, BrukerId, År, Måned, Dag , KaffeId)
+                VALUES ("{notat}", {poeng}, "{epost}", {år}, {måned}, {dag},{kaffeId})''')
+                #VALUES ("TESTINPUT", 5, test@test.test, 2022, 03, 25, 1)''')
+                #jobba litt, men ble trøtt. Det som mangler her er å få til å 
+                #gjøre om datetime objekter til integers sånn at vi kan bruke dem
+        con.commit()
+    except:
+        print("Obs, her skjedde det en feil, prøv på nytt")
+        FørsteBrukerHistorie()
+
+def printResultat():
+    try:
+        cursor.execute(f'''SELECT Kaffe.Navn, Brenneri.Navn,  Smaksnotat, Poeng, Bruker.FulltNavn, Dag, Måned, År From Kaffesmaking
+                        JOIN Kaffe on Kaffe.KaffeId = Kaffesmaking.KaffeId
+                        JOIN Bruker on Bruker.Epost = Kaffesmaking.BrukerId
+                        JOIN Brenneri on Brenneri.BrenneriId = Kaffe.BrenneriId
+                        ORDER by Kaffesmaking.KaffesmakingId DESC LIMIT 1
+                        ''')
+        res = cursor.fetchall()
+        result = res[0]
+        print(f"""
+Du har nå ført inn et smaksnotat på kaffen '{result[0]}' fra {result[1]}.
+Den ble skrevet av {result[4]} den {result[5]}. i {result[6]}. {result[7]}
+Du ga den karakter {result[3]}/10 og skrev dette om den:
+'{result[2]}'
+            """)
+    except:
+        print("Noe gikk galt i spørringen, dette er ikke din feil. ")
+    
+    
+    
+    
+
+
 def FørsteBrukerHistorie():
-    # epost = input("Hva er E-posten din? ")
-    # passord = input("Hva er passordet ditt? ")
-    # cursor.execute("""SELECT * FROM Bruker""")
-    # result = cursor.fetchall()
-    # b = False
-    # for x in result:
-    #     if(epost == x[0] and passord == x[1]):
-    #         b=True
-    # if(b):
-    #     print("Du er logget inn")
-    # else:
-    #     print("Feil brukernavn eller passord\nDu må skrive det inn på nytt")
-    #     FørsteBrukerHistorie()
-    dato = date.today()
-    år = date.year
-    måned = date.month
-    dag = date.day
+    epost = input("Hva er E-posten din? ")
+    passord = input("Hva er passordet ditt? ")
+    cursor.execute("""SELECT * FROM Bruker""")
+    result = cursor.fetchall()
+    b = False
+    for x in result:
+        if(epost == x[0] and passord == x[1]):
+            b=True
+    if(b):
+        print("Du er logget inn")
+    else:
+        print("Feil brukernavn eller passord\nDu må skrive det inn på nytt")
+        FørsteBrukerHistorie()
 
 
 
     brenneri = input("Hvilket brenneri er kaffen fra? ")
+    brenneriId = getBrenneriId(brenneri)
+
     kaffe = input("Hva er Kaffens navn? ")
+    kaffeId = getKaffeId(kaffe, brenneriId)
+
     poeng = input("Hvor mange poeng av 10 vil du gi den? ")
     notat = input("Skriv ned noen tanker om kaffen: ")
 
-    cursor.execute(f"""SELECT KaffeId from Kaffe WHERE Kaffe.Navn like '{kaffe}'""")
-    result = cursor.fetchone()
-    kaffeId = result[0]
 
-    cursor.execute(f'''INSERT INTO Kaffesmaking (Smaksnotat, Poeng, BrukerId, År, Måned, Dag , KaffeId)
-            VALUES ("{notat}", {poeng}, "test@test.test", 2022, 03, 24,{kaffeId})''')
-            #jobba litt, men ble trøtt. Det som mangler her er å få til å 
-            #gjøre om datetime objekter til integers sånn at vi kan bruke dem
-    con.commit()
+    skrivNotat(epost, kaffeId, poeng, notat)
+    printResultat()
+
+
 
 def AndreBrukerHistorie():
     cursor.execute("""SELECT FulltNavn, count(BrukerId) as Antall 
@@ -120,9 +209,11 @@ def FemteBrukerHistorie():
     
     
 def velgHistorie():
-    val = input("Hvilken brukerhistorie vil du se? (Dette er i tall mellom 1 og 5) ")
+    val = input("Hvilken brukerhistorie vil du se? (Dette er i tall mellom 1 og 5 i listen over) ")
 
-    if val == "1":
+    if val == "0":
+        lagBruker()
+    elif val == "1":
         FørsteBrukerHistorie()
     elif val == "2":
         AndreBrukerHistorie()
@@ -133,10 +224,22 @@ def velgHistorie():
     elif val =="5":
         FemteBrukerHistorie()
     else:
-        print("Dette er ugyldig brukerhistorie. Skriv inn riktig tall mellom 1 og 5")
+        print("Dette er ugyldig brukerhistorie. Skriv inn riktig tall mellom 0 og 5")
         velgHistorie()
 
 
-print("Velkommen til vårt kaffeprogram!")
+print("""
+Velkommen til vårt kaffeprogram!""")
+print("""
+Under vil du finne en liste som vil gi deg alternativer for søk
+""")
+print("""
+0. tast 0 om du vil lage en bruker?
+1. Ønsker du å lage ett nytt smaksnotat?
+2. Statistikk om hvem som har drukket mest kaffe
+3. Finne kaffen med mest verdi for pengene
+4. Søke etter kaffe med spesifikk beskrivelse (i enkelt ord)
+5. Finne kaffe som ikke innheolder behandlingsmetoden du vil unngå
+""")
 velgHistorie()
 
